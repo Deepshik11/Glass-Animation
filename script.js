6651d8d4be3a75135600ae762e04b30c716dffc5
Deepshik11 <deepdeepshik500@gmail.com>
2024-09-01 13:57:51 +0530

    hello

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ document.getElementById("scrollButton").addEventListener("click", function () {
     const targetPosition = document.documentElement.scrollWidth - window.innerWidth;
 
     let currentPosition = window.scrollX;
-    const scrollAnimation = setInterval(function () {
+    scrollAnimation1 = setInterval(function () {
         currentPosition += scrollStep;
         window.scrollTo(currentPosition, 0);
 
         if (currentPosition >= targetPosition) {
-            clearInterval(scrollAnimation);
+            clearInterval(scrollAnimation1);
         }
     }, scrollInterval);
-    function stopScrollAnimation() {
-        clearInterval(scrollAnimation);
-        window.removeEventListener('dblclick', stopScrollAnimation);
-        // window.removeEventListener('mousedown', stopScrollAnimation );
-        // window.removeEventListener('touchstart', stopScrollAnimation );
-    }
-    window.addEventListener('dblclick', stopScrollAnimation);
-    // window.addEventListener('click', stopScrollAnimation );
-    // window.addEventListener('click', stopScrollAnimation);
 });
+
+document.getElementById("stop2").addEventListener("click", function () {
+    if (scrollAnimation1) {
+        clearInterval(scrollAnimation1);
+        scrollAnimation1 = null;
+    }
+});
+let scrollAnimation1
+let scrollAnimation;
 
 document.getElementById("scrollButton2").addEventListener("click", function () {
     const scrollStep = 2;
@@ -30,7 +30,7 @@ document.getElementById("scrollButton2").addEventListener("click", function () {
     const targetPosition = document.documentElement.scrollWidth - window.innerWidth;
 
     let currentPosition = window.scrollX;
-    const scrollAnimation = setInterval(function () {
+    scrollAnimation = setInterval(function () {
         currentPosition -= scrollStep;
         window.scrollTo(currentPosition, 0);
 
@@ -38,37 +38,54 @@ document.getElementById("scrollButton2").addEventListener("click", function () {
             clearInterval(scrollAnimation);
         }
     }, scrollInterval);
-
-    function stopScrollAnimation() {
-        clearInterval(scrollAnimation);
-        window.removeEventListener('dblclick', stopScrollAnimation);
-        //         window.removeEventListener('mousedown', stopScrollAnimation);
-        //         window.removeEventListener('touchstart', stopScrollAnimation);
-    }
-    window.addEventListener('dblclick', stopScrollAnimation);
-    //     window.addEventListener('mousedown', stopScrollAnimation);
-    //     window.addEventListener('touchstart', stopScrollAnimation);
 });
+
+document.getElementById("stop1").addEventListener("click", function () {
+    if (scrollAnimation) {
+        clearInterval(scrollAnimation);
+        scrollAnimation = null;
+    }
+});
+
+
+// ----------image change-----------
 document.getElementById("scrollButton").addEventListener("click", function () {
     this.src = "Images/arrow_big-01_hover.png"
 });
-// document.getElementById("scrollButton").addEventListener("dblclick", function () {
-//     this.src = "Images/arrow_big-01.png"
-// });
+document.getElementById("stop2").addEventListener("click", function () {
+    document.getElementById("scrollButton").src = "Images/arrow_big-01.png"
+});
 document.getElementById("scrollButton2").addEventListener("click", function () {
     this.src = "Images/arrow_big-02_hover.png"
 });
-// document.getElementById("scrollButton2").addEventListener("dblclick", function () {
-//     this.src = "Images/arrow_big-02.png"
-// });
 
+document.getElementById("stop1").addEventListener("click", function () {
+    document.getElementById("scrollButton2").src = "Images/arrow_big-02.png"
+});
+
+document.getElementById("scrollButton3").addEventListener("mouseover", function () {
+    this.src = "Images/Untitled design (1).png"
+});
+document.getElementById("scrollButton3").addEventListener("mouseout", function () {
+    this.src = "Images/arrow_big-02.png"
+});
+document.getElementById("scrollButton4").addEventListener("mouseover", function () {
+    this.src = "Images/Untitled design.png"
+});
+document.getElementById("scrollButton4").addEventListener("mouseout", function () {
+    this.src = "Images/arrow_big-01.png"
+});
+
+
+// -----------color change---
 document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.getElementById("scrollButton4");
     const toggle2 = document.getElementById("scrollButton3");
     const toggle1 = document.getElementById("change")
     const container = document.querySelectorAll(".face");
-
-    const colors = ['blue', 'red', 'pink', 'green', 'orange', 'yellow', 'sky', 'violet', 'mix'];
+    const stop1 = document.getElementById("stop2")
+    const stop2=document.getElementById("stop1")
+    const colors = ['blue', 'red', 'pink', 'green', 'orange', 'yellow', 'skyblue', 'violet', 'tertiary'];
     let cindex = 0;
     let previousColor = 0;
 
@@ -76,11 +93,22 @@ document.addEventListener("DOMContentLoaded", () => {
         face.classList.add('gray');
         toggle1.innerHTML = "Default"
     });
+    stop1.classList.add('gray');
+    stop2.classList.add('gray');
+    toggle1.classList.add('gray');
 
     toggle2.addEventListener('click', () => {
         container.forEach(face => {
             face.classList.remove(colors[cindex]);
         });
+        stop1.classList.remove('gray')
+        stop1.classList.remove(colors[cindex]);
+
+        stop2.classList.remove('gray')
+        stop2.classList.remove(colors[cindex]);
+
+        toggle1.classList.remove('gray')
+        toggle1.classList.remove(colors[cindex]);
 
         cindex = (cindex - 1 + colors.length) % colors.length;
         previousColor = colors[cindex];
@@ -88,6 +116,10 @@ document.addEventListener("DOMContentLoaded", () => {
         container.forEach(face => {
             face.classList.add(previousColor);
         });
+
+        stop1.classList.add(previousColor);
+        stop2.classList.add(previousColor);
+        toggle1.classList.add(previousColor);
 
         toggle1.innerHTML = previousColor.charAt(0).toUpperCase() + previousColor.slice(1);
     });
@@ -103,11 +135,23 @@ document.addEventListener("DOMContentLoaded", () => {
             face.classList.remove('gray')
             face.classList.remove(colors[cindex]);
         });
+        stop1.classList.remove('gray');
+        stop1.classList.remove(colors[cindex]);
+
+        stop2.classList.remove('gray');
+        stop2.classList.remove(colors[cindex]);
+
+        toggle1.classList.remove('gray');
+        toggle1.classList.remove(colors[cindex]);
 
         cindex = (cindex + 1) % colors.length;
         container.forEach(face => {
             face.classList.add(colors[cindex]);
         });
+        stop1.classList.add(colors[cindex]);
+        stop2.classList.add(colors[cindex]);
+        toggle1.classList.add(colors[cindex]);
+
         toggle1.innerHTML = colors[cindex].charAt(0).toUpperCase() + colors[cindex].slice(1);
     });
 
