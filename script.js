418a06b3fafddf583bdb2047621f8385f0c0d0b2
Deepshik11 <deepdeepshik500@gmail.com>
2024-09-07 15:56:25 +0530

    asw

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-
-
 // -----------color change---
 document.addEventListener("DOMContentLoaded", () => {
-
-
     let scrollButton1 = document.getElementById("scrollButton");
     let scrollButton2 = document.getElementById("scrollButton2");
     let stopButton = document.getElementById("stop2");
     let scrollInterval;
-
     function stopScrolling() {
         if (scrollInterval) {
             clearInterval(scrollInterval);
@@ -17,7 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
             scrollButton2.src = "Images/arrow_big-02.png"
         }
     }
-
     // Function to scroll right
     function scrollRight() {
         stopScrolling();
@@ -31,9 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 50);
         this.src = "Images/arrow_big-01_hover.png"
     }
-
     // Function to scroll left
-
     function scrollLeft() {
         stopScrolling();
         scrollInterval = setInterval(() => {
@@ -45,23 +37,15 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 50);
         this.src = "Images/arrow_big-02_hover.png"
     }
-
     // Start scrolling right
     scrollButton1.addEventListener("click", scrollRight);
-
     // Start scrolling left
     scrollButton2.addEventListener("click", scrollLeft);
-
     // Stop scrolling
     stopButton.addEventListener("click", () => {
         stopScrolling();
     });
-
-
-
-
     // ----------image change-----------
-
     const toggle = document.getElementById("scrollButton4");
     const toggle2 = document.getElementById("scrollButton3");
     const toggle1 = document.getElementById("change")
@@ -70,15 +54,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const colors = ['blue', 'red', 'pink', 'green', 'orange', 'yellow', 'skyblue', 'violet', 'tertiary'];
     let cindex = 0;
     let previousColor = 0;
-
     container.forEach(face => {
         face.classList.add('gray');
         toggle1.innerHTML = "Default"
     });
     stop1.classList.add('gray');
-
     toggle1.classList.add('gray');
-
+    toggle2.addEventListener('mouseenter', () => {
+        toggle2.src = "Images/arrow_big-02_hover.png"
+    })
+    toggle.addEventListener('mouseenter', () => {
+        toggle.src = "Images/arrow_big-01_hover.png"
+    })
+    toggle2.addEventListener('mouseleave', () => {
+        toggle2.src = "Images/arrow_big-02.png"
+    })
+    toggle.addEventListener('mouseleave', () => {
+        toggle.src = "Images/arrow_big-01.png"
+    })
     toggle2.addEventListener('click', () => {
         container.forEach(face => {
             face.classList.remove(colors[cindex]);
@@ -101,7 +94,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         toggle1.innerHTML = previousColor.charAt(0).toUpperCase() + previousColor.slice(1);
     });
-
     toggle.addEventListener('click', () => {
         if (colors[0]) {
             toggle1.innerHTML = "Blue"
@@ -115,7 +107,6 @@ document.addEventListener("DOMContentLoaded", () => {
         });
         stop1.classList.remove('gray');
         stop1.classList.remove(colors[cindex]);
-
         toggle1.classList.remove('gray');
         toggle1.classList.remove(colors[cindex]);
 
@@ -125,7 +116,6 @@ document.addEventListener("DOMContentLoaded", () => {
         });
         stop1.classList.add(colors[cindex]);
         toggle1.classList.add(colors[cindex]);
-
         toggle1.innerHTML = colors[cindex].charAt(0).toUpperCase() + colors[cindex].slice(1);
     });
 
